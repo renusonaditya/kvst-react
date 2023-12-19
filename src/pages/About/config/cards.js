f3cb31f69066e6f9baab35ea223cf729b0554804
adityaarakal <adityararakal@gmail.com>
2023-12-19 17:46:54 +0530

    modified about page content alignment

--- src/pages/About/config/cards.js
+++ src/pages/About/config/cards.js
@@ -9,9 +9,6 @@ export const cards = [
         image: Afull,
         subtitle: `Sri Kashi Vishwanath temple is an ancient Hindu temple located in the
             Medchal Village on a small hill rock in the state of Telangana.
-            <br />
-            <br />
-            <br />
             Idols present in the temple: Sri Kashi Vishwanath Swamy, Annapurna Devi,
             Vigneshwara swamy, Laxmi Narayana swamy, Surya Bhagwan, Anjaneya swamy,
             Nandeeshwara swamy.`,
@@ -47,27 +44,27 @@ export const cards = [
         devotees of all <br />
         communities into the temple, the temple was constructed by his
         own money <br />
-        without collecting funds from anyone. During the time of
-        construction, <br />
-        there were many hurdles created by Nizam government in order
-        to stop the <br />
-        construction of temple. As the Razakars used to demolish the
-        temple in the <br />
-        day time, the construction used to take place during the night
-        time. As this <br />
-        continues for few days, Lord Shiva appeared in the dream of
-        Kandukuri <br />
-        Venkata Ramayya and said “Do not fear constructing my temple.
-        Continue <br />
-        constructing the temple during day time without any fear”.
-        Temple was almost <br /> completed and Nizam army handcuffed Venkata Ramayya and tried
-        to take him into the prison. Immediately the handcuffs turned
-        to flower garland. `,
+        without collecting funds from anyone.`,
         description: `
-       <br />
+        During the time of
+        construction,
+        there were many hurdles created by Nizam government in order
+        to stop the
+        construction of temple. As the Razakars used to demolish the
+        temple in the
+        day time, the construction used to take place during the night
+        time. As this 
+        continues for few days, Lord Shiva appeared in the dream of
+        Kandukuri
+        Venkata Ramayya and said “Do not fear constructing my temple.
+        Continue 
+        constructing the temple during day time without any fear.
+        Temple was almost completed and Nizam army handcuffed Venkata Ramayya and tried
+        to take him into the prison. Immediately the handcuffs turned
+        to flower garland. 
         There after he was sent to jail in Mumbai with respect for few
         days. He has completed the construction of temple using his
-        influence while he was in the jail. <br />
+        influence while he was in the jail.
         Later on, he was released from the jail. He continued
         organizing Nitya Puja and Performed several Homas in the
         temple.<br /><br/>  Idols situated in the temple were sculpted in the Dashashwamedh
@@ -84,10 +81,12 @@ export const cards = [
         Sri Kashi Vishwanath temple has 2 entrances in 2 directions.
         The Main entrance of the temple is east facing dwara which is called as Thoorpu dwara.
         It was designed as per Agama Shastra.<br />
-        Main door of the temple was made during the construction of temple which is about 20 feet height and 13 feet width. <br/>
-        2nd entrance of the temple is north facing dwara which is called as Uttara Dwara. It glows holy radiance of Sri Kashi Vishwanath Temple in Varanasi which is about 1000kms away from north facing door of the temple.
         `,
         description: `
+        Main door of the temple was made during the construction of temple which is about 20 feet height and 13 feet width. <br/>
+        2nd entrance of the temple is north facing dwara which is called as Uttara Dwara. It glows holy radiance of Sri Kashi Vishwanath Temple in Varanasi which is about 1000kms away from north facing door of the temple.
+        <br />
+        <br />
         <h2>DIETIES IN THE TEMPLE:</h2>
         <h3>Deities present in the temple:</h3>
         Sri Kashi Vishwanath Swamy, Annapurna Devi, Vigneshwara swamy, Laxmi Narayana swamy, Surya Bhagwan, Nandeeshwara swamy. <br />
